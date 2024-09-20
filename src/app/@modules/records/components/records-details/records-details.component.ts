58bf829a57fe67b1b2948b5f14817525689c845a
Ivan <ivojlo2002@gmail.com>
2024-09-20 11:54:39 +0200

    changed default route to borrowed, and created records-details component

---
+++ src/app/@modules/records/components/records-details/records-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-records-details',
+  templateUrl: './records-details.component.html',
+  styleUrl: './records-details.component.scss'
+})
+export class RecordsDetailsComponent {
+
+}
