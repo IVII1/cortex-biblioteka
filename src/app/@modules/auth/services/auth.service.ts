6b46e98f7f2d10397e4ef78b7ba3d63903759ce7
Ivan <ivojlo2002@gmail.com>
2024-10-03 15:44:20 +0200

    added logout method

--- src/app/@modules/auth/services/auth.service.ts
+++ src/app/@modules/auth/services/auth.service.ts
@@ -1,17 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+  ) {}
 
   login(data: any) {
     const url = `${environment.apiLogin}`;
     const headers = new HttpHeaders().set('Authorization', environment.apiKey);
     return this.httpClient.post(url, data, { headers });
+  }
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/auth']);
   }
 }
